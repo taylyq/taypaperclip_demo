a65aa1dfaf2c8a528498100c130cb6c7627d436a
tay <taylyq@yahoo.com>
2015-08-21 15:55:07 -0500

    try new env 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
   :storage => :s3,
   :s3_host_name => "s3-eu-west-1.amazonaws.com",
   :s3_credentials => {
-  :bucket => ENV["S3_BUCKET_NAME"],
-  :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
-  :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
+  :bucket => ENV["paperclipdemodevelopment"],
+  :access_key_id => ENV["AKIAIRC7VNAH7ZGHUX2A"],
+  :secret_access_key => ENV["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"]
  }
 end
